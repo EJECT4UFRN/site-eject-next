102c4b153c3912c8c48f4acf0d1ae0c9223853ab
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-03-03 23:11:26 -0300

    fix: correção no component VideoWindow

--- src/components/VideoWindow/index.tsx
+++ src/components/VideoWindow/index.tsx
@@ -4,7 +4,7 @@ export function VideoWindow() {
   return (
     <Container>
       <iframe
-        src="https://www.youtube.com/embed/O_ioE-7upWo"
+        src="https://www.youtube.com/embed/BOcu63fzLEQ"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
