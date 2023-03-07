8202d52a50ba72992c0513090c76d89f020997f9
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-03-06 21:47:02 -0300

    refactor: atualização na rota API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_DEVELOPER = 'http://127.0.0.1:8000/api/';
-const API_PRODUCTION = 'http://apisite.ejectufrn.com.br/api/';
+const API_PRODUCTION = 'https://apisite.ejectufrn.com.br/api/';
 
 
 const api = axios.create({
