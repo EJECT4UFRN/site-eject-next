d946e1731718bc446df80d297d628c483fb14dad
PedroHenrique06 <pedrosales2806@gmail.com>
2026-09-16 19:28:03 -0300

    cleanup: reoganização das configs de api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,12 +3,11 @@ import axios from 'axios';
 const API_DEVELOPER = 'http://127.0.0.1:8000/api/';
 const API_PRODUCTION = 'https://apisite.ejectufrn.com.br/api/';
 
-
 const api = axios.create({
-  baseURL: API_PRODUCTION,
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? API_PRODUCTION,
   auth: {
-    username: "client_site_eject",
-    password: "Senha@frontEJECT21"
+    username: process.env.NEXT_PUBLIC_API_USER ?? "client_site_eject",
+    password: process.env.NEXT_PUBLIC_API_PASSWORD ?? "Senha@frontEJECT21"
   }
 });
 
